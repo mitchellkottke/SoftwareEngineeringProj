ffd9c28f5c28bc4eff3c8dd353919fc6e9b52d01
Sydney Matthys <matth416@d.umn.edu>
2018-03-27 10:48:28 -0500

    Navigation Menu Code

--- app/app/src/main/java/com/example/liz/interviewapp/ResourcesActivity.java
+++ app/app/src/main/java/com/example/liz/interviewapp/ResourcesActivity.java
@@ -1,8 +1,13 @@
 package com.example.liz.interviewapp;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.MenuItem;
 
 
 /**
@@ -10,11 +15,51 @@ import android.os.Bundle;
  * Might have to figure out how to access these links, e.g. open chrome,
  * or it might do it automatically. -ghutch
  */
-public class ResourcesActivity extends AppCompatActivity {
+public class ResourcesActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resources);
+        mDrawerLayout= (DrawerLayout) findViewById(R.id.nav_drawer);
+        mToggle = new ActionBarDrawerToggle(ResourcesActivity.this, mDrawerLayout, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView navigationView=(NavigationView)findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.nav_home){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+        if (id == R.id.nav_recordVideo){
+            Intent intent = new Intent(this, RecordVideoActivity.class);
+            startActivity(intent);
+        }
+        if (id == R.id.nav_flashcards){
+            Intent intent = new Intent(this, FlashcardsActivity.class);
+            startActivity(intent);
+        }
+        if (id == R.id.nav_resources){
+            Intent intent = new Intent(this, ResourcesActivity.class);
+            startActivity(intent);
+        }
+        return false;
     }
 }
