b18b2806dbc9f8181e614422ad6d125b8c7f6294
Liz Slivinski <slivi005@d.umn.edu>
2018-04-08 12:58:33 -0500

    google log in screen now links to main activity

--- app/app/src/main/java/com/example/liz/interviewapp/LogIn.java
+++ app/app/src/main/java/com/example/liz/interviewapp/LogIn.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -18,6 +19,7 @@ import com.google.android.gms.common.api.GoogleApiClient;
 public class LogIn extends AppCompatActivity implements View.OnClickListener, GoogleApiClient.OnConnectionFailedListener {
 
     private SignInButton signIn;
+    private Button signOut;
     private static final int REQ_CODE = 9001;
     GoogleApiClient googleApiClient;
 
@@ -29,13 +31,20 @@ public class LogIn extends AppCompatActivity implements View.OnClickListener, Go
         setContentView(R.layout.activity_log_in);
         signIn = (SignInButton)findViewById(R.id.googleLogIn);
         signIn.setOnClickListener(this);
+        signOut = (Button)findViewById(R.id.googleLogOut);
         GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         googleApiClient = new GoogleApiClient.Builder(this).enableAutoManage(this, this).addApi(Auth.GOOGLE_SIGN_IN_API, signInOptions).build();
     }
 
     @Override
     public void onClick(View view) {
-        signIn();
+
+        switch(view.getId()) {
+            case R.id.googleLogIn:
+                signIn();
+                break;
+        }
+
 
     }
 
@@ -50,10 +59,22 @@ public class LogIn extends AppCompatActivity implements View.OnClickListener, Go
 
     }
 
+    private void signOut() {
+
+    }
+
+    private void updateUI(boolean isLogin) {
+        Intent main = new Intent(this, MainActivity.class);
+        startActivity(main);
+
+    }
+
     //this is where we can display name, prof pic, etc
+    //go to 27:06 on tutorial
     private void handleResult(GoogleSignInResult result){
         if(result.isSuccess()) {
             GoogleSignInAccount account = result.getSignInAccount();
+            updateUI(true);
         }
 
     }
