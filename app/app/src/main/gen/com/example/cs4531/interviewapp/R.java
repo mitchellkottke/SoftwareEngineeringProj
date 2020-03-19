ed4a2004b3fbbf11d5964220578cc2913c8569c6
Mitchell Kottke <kottk055@d.umn.edu>
2020-03-19 16:22:37 -0500

    Checking how commit works

---
+++ app/app/src/main/gen/com/example/cs4531/interviewapp/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.example.cs4531.interviewapp;
+
+/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
+public final class R {
+}
